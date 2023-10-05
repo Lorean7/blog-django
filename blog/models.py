d7a33dd0315d3fd5d3c9294f8a620089ba9eb0d6
Lorean7 <oleglone7@gmail.com>
2023-10-05 16:35:36 +0300

    Создана модель POST

---
+++ blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+# Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'DRAFT'
+        PUBLISHED = 'PB', 'PUBLISHED'
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(
+                fields=['-publish']
+            ),
+        ]
+
+    def __str__(self):
+        return self.title
